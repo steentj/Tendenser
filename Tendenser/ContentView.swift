367d6b0015637dca7afa146f3e8ba51528412764
steen <steentj@hotmail.com>
2023-07-06 11:47:11 +0200

    Initial Commit

---
+++ Tendenser/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tendenser
+//
+//  Created by Steen Thrane Jacobsen on 06/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tendenser/TendenserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TendenserApp.swift
+//  Tendenser
+//
+//  Created by Steen Thrane Jacobsen on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TendenserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
