e2039cbd1e063b02885298a631a4b6fa4e234f10
Steen Thrane Jacobsen <steentj@hotmail.com>
2024-02-22 15:47:46 +0100

    SwiftData version der virker nogenlunde. Masser af finpudsning tilbage

---
+++ Tendenser/MaalingerGraf.swift
@@ -0,0 +1,18 @@
+//
+//  MaalingerGraf.swift
+//  Tendenser
+//
+//  Created by Steen Thrane Jacobsen on 15/02/2024.
+//
+
+import SwiftUI
+
+struct MaalingerGraf: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaalingerGraf()
+}
---
+++ Tendenser/Preview Content/TendensPreviewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TendensPreviewExtension.swift
+//  Tendenser
+//
+//  Created by Steen Thrane Jacobsen on 13/02/2024.
+//
+
+import Foundation
